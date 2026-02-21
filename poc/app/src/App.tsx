0b93d2d86632c625ab4bb983bb7860497c45ab22
tarrin <tarrinc3@gmail.com>
2026-02-21 18:00:58 +0000

    Changed to raw Websockets in client.

--- poc/app/src/App.tsx
+++ poc/app/src/App.tsx
@@ -26,7 +26,6 @@ export default function App() {
   }
 
   const handleNodeClick = (nodeId: string) => {
-    console.log("Selected node set:", nodeId);
     setSelectedNode(nodeId);
   };
 
--- poc/app/src/providers/SocketProvider.tsx
+++ poc/app/src/providers/SocketProvider.tsx
@@ -1,23 +1,23 @@
 import { createContext, useState, useEffect } from 'react';
-import io, { Socket } from "socket.io-client";
+import { WebSocket } from 'ws';
 
-export const SocketContext = createContext<Socket | undefined>(undefined);
+export const SocketContext = createContext<WebSocket | undefined>(undefined);
 
 function SocketProvider({ children }: { children?: React.ReactNode }) {
-  const [socket, setSocket] = useState<Socket>();
+  const [socket, setSocket] = useState<WebSocket>();
     
   useEffect(() => {
     if (socket) {
       return;
     }
 
-    const ws = io(`http://localhost:8000`, { withCredentials: true });
-    ws.on("connect", () => {
+    const ws = new WebSocket('ws://www.host.com/path');
+    ws.on("open", () => {
       setSocket(ws);
     });
 
     return () => {
-      ws?.disconnect();
+      ws?.close();
     }
   }, [socket]);
 
