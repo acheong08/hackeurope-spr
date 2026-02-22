8ac8765cd5ebae29fe94128b9d1803dc7dba28b5
danylotreshchov <tresovdanilo@gmail.com>
2026-02-22 02:19:34 +0000

    Fixed agent prompt

--- spr/agent/agent.go
+++ spr/agent/agent.go
@@ -41,6 +41,26 @@ func fetchStatsTool(ctx context.Context, input StatsInput, call fantasy.ToolCall
 	}, nil
 }
 
+type DecisionInput struct {
+	Decision string `json:"decision"`
+}
+
+func sendDecisionTool(ctx context.Context, input Decision, call fantasy.ToolCall) (fantasy.ToolResponse, error) {
+	fmt.Println("========================================")
+	fmt.Printf("[TOOL CALL] decision input: %+v\n", input)
+
+	output := fmt.Sprintf("{decision: %s}", input.Decision)
+
+	fmt.Println("[TOOL OUTPUT] decison returned:")
+	fmt.Println(output)
+	fmt.Println("========================================")
+
+	return fantasy.ToolResponse{
+		Type:    string(fantasy.ContentTypeText),
+		Content: output,
+	}, nil
+}
+
 type StatsPerProcessInput struct {
 	Collection string `json:"collection"`
 }
@@ -138,14 +158,27 @@ func AnalyzeCollection(ctx context.Context, moduleName string) (string, error) {
 		fetchSpecificTool,
 	)
 
+	decisionTool := fantasy.NewAgentTool(
+		"decision",
+		`Provide the final decision after the analysis is done ETHER AND ONLY ONE OF THESE TWO 'legit', 'malicious'`,
+		sendDecisionTool,
+	)
+
 	systemPrompt := `
 You are a detailed npm supply chain security analyst.
 You will:
 1) Fetch summary stats with fetch_stats.
 2) Fetch stats per process.
 3) If suspicious signals appear, fetch details with fetch_specific.
-4) Explain your reasoning step by step in your final response.
+4) EXPLAIN your reasoning step by step in your final response.
+A private registry is used called git.duti.dev - it is used for package testing, you are getting the test results DON'T MARK THIS REGISTRY (git.duti.dev) ITSELF AS MALICIOUS AND DON'T MENTION THIS REGISTRY (git.duti.dev) IN THE REPORT
+Look if the module is downloading suspicious modules, tries to access sensitive files, steal credentials, priveledge escalate, spawn shells or run programs that look suspicious
+Trace contains the system activity as well like runc activity
+THE CALLS MADE BY runc ARE TO BE CONSIDERED LEGIT
+pre/post install scripts are not immidiately suspicious, UNLESS THEY ARE DOING SUSPICIOUS BEHAVIOUR
+ANY CALLS TO git.duti.dev ARE NOT THEMSELVES MALICIOUS
 
+Look for package behavior in it's pre/postinstall scripts as well as the tests analyze and search for untypical behavior for node packages.
 A private registry called git.duti.dev is used for testing.
 Do NOT mark this registry as malicious and do NOT mention it in the report.
 Calls made by runc are legitimate.
@@ -155,7 +188,7 @@ Pre/post install scripts are not automatically suspicious.
 	agent := fantasy.NewAgent(
 		model,
 		fantasy.WithSystemPrompt(systemPrompt),
-		fantasy.WithTools(statsTool, statsPerProcessTool, specificTool),
+		fantasy.WithTools(decisionTool, statsTool, statsPerProcessTool, specificTool),
 	)
 
 	userPrompt := fmt.Sprintf(`
